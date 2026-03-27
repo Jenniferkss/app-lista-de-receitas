79b2f4245b1d42fb2d1bcd33e61e23115d8c3ed2
Ana Julia <ana.j.ferreir15@aluno.senai.br>
2026-03-27 15:23:00 -0300

    Feat: Adicona a estrutura do app mobile

--- app-receitas/app-receitas/App.js
+++ app-receitas/app-receitas/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, StatusBar } from 'react-native';
 
-export default function App() {
+const receitas = [
+  {id: 1, titulo: 'Morango Haute Sélection', image: {uri: ''}},
+  {id: 2, titulo: 'Morango Haute Sélection', image: {uri: ''}},
+  {id: 3, titulo: 'Morango Haute Sélection', image: {uri: ''}},
+  {id: 4, titulo: 'Morango Haute Sélection', image: {uri: ''}},
+];
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={StyleSheet.container}>
+      <StatusBar barStyle="light-content"/>
+
+      <View style={StyleSheet.header}>
+      <View style={StyleSheet.logoCircle}>
+
+      <Image source={require('./assets/logo.png')}
+      style={style.logoImage}   
+    />
+      </View>
+      </View>
+
+      <View style={styles.content}>
+        <Text style={styles.titulo}>Receitas atuais</Text>
+
+        <View style={styles.listaContainer}></View>
+        {receitas.map((item) =>(
+          <TouchableOpacity key={item.id} activeOpacity={0.8} style={styles.cardWrapper}>
+            <ImageBackground
+            source={item.image}
+            style={styles.cardImage}
+            imageStyle={styles.cardImageRadius}></ImageBackground>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.addButtonText}>Adicione uma nova receita</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
